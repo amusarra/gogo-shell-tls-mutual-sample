6b9c767031c0a1cdbd8d19834dad642ef95bfa7d
Antonio Musarra <antonio.musarra@gmail.com>
2020-02-21 23:09:07 +0100

    Added copyright header

--- src/main/java/it/dontesta/labs/liferay/security/tls/auth/gogoshell/HTTPSProtectedResourcesCommand.java
+++ src/main/java/it/dontesta/labs/liferay/security/tls/auth/gogoshell/HTTPSProtectedResourcesCommand.java
@@ -1,3 +1,29 @@
+/**
+ * MIT License
+ *
+ * Gogo Shell - Command for getting HTTPS protected resources
+ *
+ * Copyright (C) 2020 Antonio Musarra's Blog
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package it.dontesta.labs.liferay.security.tls.auth.gogoshell;
 
 import com.liferay.portal.kernel.log.Log;
@@ -21,11 +47,11 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class HTTPSProtectedResourcesCommand {
 
-	public void getProtectedResource(String URI) throws Exception {
+	public void getProtectedResource(String uri) throws Exception {
 		String response = null;
 
 		try {
-			response = _httpClient.URLtoString(URI);
+			response = _httpClient.URLtoString(uri);
 
 			if (_log.isInfoEnabled()) {
 				_log.info("Response: " + response);
@@ -40,7 +66,8 @@ public class HTTPSProtectedResourcesCommand {
 
 			throw new Exception(
 				String.format(
-					"They occurred in the service call. {EndPoint: %s}", URI),
+					"They occurred in the service call. {EndPoint: %s} - {Error Message: %s}",
+					uri, ioe.getMessage()),
 				ioe);
 		}
 	}
